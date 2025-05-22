2427d75cbb59162567d28fb3b733722cdfe2a430
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-05-22 12:13:36 +0530

    Update wiredecl.v

--- Basics/wiredecl.v
+++ Basics/wiredecl.v
@@ -1,1 +1,15 @@
 
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+wire in1,in2,out1,out2;
+    assign in1 = a&b;
+    assign in2 = c&d;
+    assign out = in1|in2;
+    assign out_n = ~out;
+endmodule
